ce27a4d71219a98507ef5381d91639ebadf31870
kili196 <kilian.baier16@gmail.com>
2024-10-05 18:15:42 +0200

    added fetch from api method

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 async function getWeatherDataFromApi(city) {
     const apiKey = "XT56GNF3GWHBFCPHT2D7PFWTF";
 
-    const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`);
 
+    if (!response.ok) {
+        throw new Error("Error in fetching data from api!")
+    }
+
+    const data = await response.json();
+
+    return data;
 }
+
+
+console.log(getWeatherDataFromApi("london"));
